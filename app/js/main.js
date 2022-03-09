f96d351f99f9285ba5533c2b70786403076c0fb9
Vadim Losenkov <vprog0729@gmail.com>
2022-03-09 13:54:58 +0300

    start popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function () {
+  $('.open-popup').magnificPopup({
+    type: 'inline',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+      beforeOpen: function () {
+        this.st.mainClass = this.st.el.attr('data-effect');
+      }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+  
   $('[data-header="toggler"]').on('click', function (e) {
     $('.header-mobile').toggleClass('open');
   });
